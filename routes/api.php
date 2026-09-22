8388d3bd239fc767187f3b31b6f083c2461941f2
FajarrKurniawan9 <fk357421@gmail.com>
2026-09-22 17:24:59 +0700

    Add Guru resource with CRUD operations and migration

--- routes/api.php
+++ routes/api.php
@@ -89,6 +89,7 @@
 */
 
 use App\Http\Controllers\Api\KartuPelajarController;
+use App\Http\Controllers\Api\GuruController;
 use App\Http\Controllers\Api\KelasController;
 use App\Http\Controllers\Api\SiswaController;
 use Illuminate\Http\Request;
@@ -106,3 +107,6 @@ Route::apiResource('siswa', SiswaController::class);
 
 // CRUD Kartu Pelajar: GET|POST /kartu-pelajar, GET|PUT|PATCH|DELETE /kartu-pelajar/{kartu_pelajar}
 Route::apiResource('kartu-pelajar', KartuPelajarController::class);
+
+// CRUD Guru: GET|POST /guru, GET|PUT|PATCH|DELETE /guru/{guru}
+Route::apiResource('guru', GuruController::class);
